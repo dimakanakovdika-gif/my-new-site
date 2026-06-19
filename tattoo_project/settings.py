ec05dfd9e4ae5b6cf75b59967654095dd8101449
ИСП-22-4 Канаков Дмитрий Михайлович <8357@stud.pgt.su>
2026-06-19 15:38:43 +0700

    Загрузка Django-проекта

--- tattoo_project/settings.py
+++ tattoo_project/settings.py
@@ -132,8 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-# Добавьте в конец settings.py
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Настройки кэширования
 CACHES = {
@@ -160,10 +163,4 @@ if not DEBUG:
     SECURE_CONTENT_TYPE_NOSNIFF = True
     # Настройки email (для тестирования)
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Письма выводятся в консоль
-# Для реальной отправки используйте:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.yandex.ru'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'ваш_email'
-# EMAIL_HOST_PASSWORD = 'ваш_пароль'
+
