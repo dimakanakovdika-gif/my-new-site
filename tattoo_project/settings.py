6ba1b9eb4b9e03d1a4893e252ada643952ba129f
ИСП-22-4 Канаков Дмитрий Михайлович <8357@stud.pgt.su>
2026-06-19 16:19:55 +0700

    Fix deployment config

--- tattoo_project/settings.py
+++ tattoo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a^*ftf4tsql#^83^m*ndyp!t4esa)f3=$mmi-agv80=)u5&!72
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh', 'localhost', '127.0.0.1']
 
 
 # Application definition
